daf49e2ba159213f27fafe9f29817df30416ff56
flashscript <rubensaircortez@gmail.com>
2020-07-29 09:45:53 -0400

    Update models.py

--- skynet/user/models.py
+++ skynet/user/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+	username = models.CharField('username', max_length=150, unique=False,)
 	email = models.EmailField('email address', unique=True)
 	USERNAME_FIELD = 'email'
 	REQUIRED_FIELDS = ['username']
